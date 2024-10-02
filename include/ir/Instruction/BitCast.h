6f7bd993f4258322a36d7679b2ba64d94eb42a37
wtz <wangtz0607@outlook.com>
2024-10-02 13:01:37 +0800

    [ir] `BitCast`: Support constant folding

--- include/ir/Instruction/BitCast.h
+++ include/ir/Instruction/BitCast.h
@@ -6,9 +6,16 @@
 #include <unordered_set>
 #include <utility>
 
+#include "ir/Constant.h"
+#include "ir/Constant/FloatingConstant.h"
+#include "ir/Constant/IntegerConstant.h"
+#include "ir/Constant/PoisonValue.h"
 #include "ir/Instruction/Operator.h"
 #include "ir/InstructionVisitor.h"
 #include "ir/Type.h"
+#include "ir/Type/FloatingType.h"
+#include "ir/Type/IntegerType.h"
+#include "ir/Type/Ptr.h"
 #include "ir/Use.h"
 #include "ir/Value.h"
 #include "utils/Memory.h"
@@ -28,6 +35,15 @@ public:
     std::unordered_set<const UseBase *> operands() const override {
         return {&value()};
     }
+
+    bool isFoldable() const override {
+        return ((dynamic_cast<const IntegerConstant *>(&*value()) || dynamic_cast<const FloatingConstant *>(&*value()))
+                    && (dynamic_cast<const IntegerType *>(&*type()) || dynamic_cast<const FloatingType *>(&*type()))
+                    && *type() != Ptr())
+            || dynamic_cast<const PoisonValue *>(&*value());
+    }
+
+    std::unique_ptr<Constant> fold() const override;
 
     void accept(InstructionVisitor &visitor) override {
         visitor.visitBitCast(*this);
---
+++ lib/ir/Instruction/BitCast.cpp
@@ -0,0 +1,141 @@
+#include "ir/Instruction/BitCast.h"
+
+#include <cstdint>
+#include <memory>
+#include <optional>
+
+#include "common/ops/BitCast.h"
+#include "ir/Constant.h"
+#include "ir/Constant/DoubleConstant.h"
+#include "ir/Constant/FloatConstant.h"
+#include "ir/Constant/I16Constant.h"
+#include "ir/Constant/I1Constant.h"
+#include "ir/Constant/I32Constant.h"
+#include "ir/Constant/I64Constant.h"
+#include "ir/Constant/I8Constant.h"
+#include "ir/Constant/PoisonValue.h"
+#include "ir/ConstantVisitor.h"
+#include "ir/Type/Double.h"
+#include "ir/Type/Float.h"
+#include "ir/Type/I1.h"
+#include "ir/Type/I16.h"
+#include "ir/Type/I32.h"
+#include "ir/Type/I64.h"
+#include "ir/Type/I8.h"
+#include "ir/TypeVisitor.h"
+
+using namespace ir;
+
+namespace {
+
+template <typename To, typename ToConst, typename ToTy>
+class ConstantVisitorImpl final : public ConstantVisitor {
+public:
+    std::unique_ptr<Constant> takeResult() {
+        return std::move(result_.value());
+    }
+
+    void visitI1Constant(const I1Constant &value) override {
+        visit(value);
+    }
+
+    void visitI8Constant(const I8Constant &value) override {
+        visit(value);
+    }
+
+    void visitI16Constant(const I16Constant &value) override {
+        visit(value);
+    }
+
+    void visitI32Constant(const I32Constant &value) override {
+        visit(value);
+    }
+
+    void visitI64Constant(const I64Constant &value) override {
+        visit(value);
+    }
+
+    void visitFloatConstant(const FloatConstant &value) override {
+        visit(value);
+    }
+
+    void visitDoubleConstant(const DoubleConstant &value) override {
+        visit(value);
+    }
+
+    void visitNullPtrConstant(const NullPtrConstant &) override {
+        result_.emplace(ToTy().zeroValue());
+    }
+
+private:
+    std::optional<std::unique_ptr<Constant>> result_;
+
+    template <typename FromConst>
+    void visit(const FromConst &value) {
+        result_.emplace(std::make_unique<ToConst>(ops::BitCast<To>()(value.value())));
+    }
+};
+
+class TypeVisitorImpl final : public TypeVisitor {
+public:
+    explicit TypeVisitorImpl(const Constant &value) : value_(value) {}
+
+    std::unique_ptr<Constant> takeResult() {
+        return std::move(result_.value());
+    }
+
+    void visitI1(const I1 &) override {
+        visit<bool, I1Constant, I1>();
+    }
+
+    void visitI8(const I8 &) override {
+        visit<int8_t, I8Constant, I8>();
+    }
+
+    void visitI16(const I16 &) override {
+        visit<int16_t, I16Constant, I16>();
+    }
+
+    void visitI32(const I32 &) override {
+        visit<int32_t, I32Constant, I32>();
+    }
+
+    void visitI64(const I64 &) override {
+        visit<int64_t, I64Constant, I64>();
+    }
+
+    void visitFloat(const Float &) override {
+        visit<float, FloatConstant, Float>();
+    }
+
+    void visitDouble(const Double &) override {
+        visit<double, DoubleConstant, Double>();
+    }
+
+private:
+    const Constant &value_;
+    std::optional<std::unique_ptr<Constant>> result_;
+
+    template <typename To, typename ToConst, typename ToTy>
+    void visit() {
+        ConstantVisitorImpl<To, ToConst, ToTy> visitor;
+        value_.accept(visitor);
+        result_.emplace(visitor.takeResult());
+    }
+};
+
+std::unique_ptr<Constant> foldImpl(const BitCast &I) {
+    const Constant &value = static_cast<const Constant &>(*I.value());
+    if (dynamic_cast<const PoisonValue *>(&value)) {
+        return std::make_unique<PoisonValue>(I.type());
+    }
+    TypeVisitorImpl visitor(value);
+    I.type()->accept(visitor);
+    return visitor.takeResult();
+}
+
+} // namespace
+
+std::unique_ptr<Constant> BitCast::fold() const {
+    return foldImpl(*this);
+}
